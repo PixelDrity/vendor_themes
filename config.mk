bfcee20c0d5cfb07489a70ebc8fbbb805c069482
Basiczero <57948451+Basiczero@users.noreply.github.com>
2019-11-22 10:50:48 +0700

    intial repos

---
+++ config.mk
@@ -0,0 +1,28 @@
+PRODUCT_PACKAGES += \
+    FaceLockDark \
+    SettingsDark \
+    SettingsIntelligenceDark \
+    SystemDark \
+    SystemNotificationsDark \
+    SystemBlack \
+    SystemNotificationsBlack \
+    SystemUIDark \
+    SystemUIBlack \
+    DocumentsUIDark \
+    DocumentsUIBlack \
+    UpdatesDark \
+    UpdatesBlack
+
+# Accents
+PRODUCT_PACKAGES += \
+    BrownAccent \
+    TealAccent \
+    BlackAccent \
+    YellowAccent \
+    RedAccent \
+    PurpleAccent \
+    PinkAccent \
+    OrangeAccent \
+    GreenAccent \
+    WhiteAccent \
+    CyanAccent \
